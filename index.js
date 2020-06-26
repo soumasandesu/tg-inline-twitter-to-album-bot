377ddcb1237e50abd5aa1b259e3188bf8a140c92
soumasandesu <hkgsherlock@gmail.com>
2020-06-26 12:09:09 +0800

    proto

--- index.js
+++ index.js
@@ -7,45 +7,22 @@ const {
     PORT = "3001"
 } = process.env;
 
-// const Twitter = require("twitter");
-const Restify = require("restify");
-const Slimbot = require("slimbot");
-const UrlJoin = require("url-join");
+const TelegramBot = require('node-telegram-bot-api');
+const UrlJoin = require('url-join');
 
-// const client = new Twitter({
-//     consumer_key: TWITTER_CONSUMER_KEY,
-//     consumer_secret: TWITTER_CONSUMER_SECRET,
-// });
-
-const slimbot = new Slimbot(TELEGRAM_TOKEN);
-
-const server = Restify.createServer();
-server.use(Restify.plugins.bodyParser());
-server.post(SERVER_TG_REQUEST_API_ENTRY, (req, res, next) => {
-    const update = req.body || {};
-    console.log(JSON.stringify(update));
-    if (update.message) {
-        const { message } = update;
-        slimbot.sendMessage(
-            message.chat.id, 
-            "Message received:\n" +
-            `from: ${message.from.username}\n`+
-            `text: ${message.text}\n` +
-            `time: ${message.date}`
-        );
-    }
-    return next();
+const bot = new TelegramBot(TELEGRAM_TOKEN, {
+    webHook: {
+        port: PORT,
+    },
 });
-const tgUrl = UrlJoin(BASE_URL, SERVER_TG_REQUEST_API_ENTRY);
-console.log(`setting telegram webhook url to ${tgUrl}`);
-slimbot.setWebhook({
-    url: tgUrl,
-});
-server.listen(PORT, () => console.log(`listening to port ${PORT}`));
+bot.setWebHook(UrlJoin(BASE_URL, `/bot${TELEGRAM_TOKEN}`));
 
-const handleShutdown = (sig) => () => {
-    console.info(`${sig} signal received.`);
-    slimbot.deleteWebhook(() => console.log("Telegram webhook deleted"));
-    server.close(() => console.log('Http server closed.'));
-};
-["SIGINT", "SIGTERM"].forEach((sig) => process.on(sig, handleShutdown(sig)));
+bot.on("message", (msg) => {
+    bot.sendMessage(
+        msg.chat.id,
+        "Message received:\n" +
+        `from: ${message.from.username}\n`+
+        `text: ${message.text}\n` +
+        `time: ${message.date}`
+    );
+})
