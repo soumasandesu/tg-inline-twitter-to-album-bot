1e8378ba56312a66e723c0bdef911f7881360471
soumasandesu <hkgsherlock@gmail.com>
2020-06-26 11:19:47 +0800

    slimbot

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const {
 
 // const Twitter = require("twitter");
 const Restify = require("restify");
+const Slimbot = require("slimbot");
 const UrlJoin = require("url-join");
 
 // const client = new Twitter({
@@ -16,17 +17,21 @@ const UrlJoin = require("url-join");
 //     consumer_secret: TWITTER_CONSUMER_SECRET,
 // });
 
+const slimbot = new Slimbot(TELEGRAM_TOKEN);
+
 const server = Restify.createServer();
-server.use(Restify.bodyParser());
+server.use(Restify.plugins.bodyParser());
 server.post(SERVER_TG_REQUEST_API_ENTRY, (req, res, next) => {
     const context = req.body;
     if (update.message) {
         slimbot.sendMessage(message.chat.id, "Message received");
     }
     return next();
-})
+});
+const tgUrl = UrlJoin(BASE_URL, SERVER_TG_REQUEST_API_ENTRY);
+console.log(`setting telegram webhook url to ${tgUrl}`);
 slimbot.setWebhook({
-    url: UrlJoin(BASE_URL, SERVER_TG_REQUEST_API_ENTRY),
+    url: tgUrl,
 });
 server.listen(PORT);
 console.log(`listening to port ${PORT}`);
